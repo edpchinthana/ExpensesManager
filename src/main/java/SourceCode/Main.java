08f187392e40d40ae5d8330bac9369cc18fcc895
edpchinthana <edpchinthana@outlook.com>
2020-02-15 22:53:39 +0530

    Adding resource files

--- src/main/java/SourceCode/Main.java
+++ src/main/java/SourceCode/Main.java
@@ -1,9 +1,12 @@
 
 package SourceCode;
 
+import GUI.MainMenu;
+
 
 public class Main {
     public static void main(String args[]){
-        
+        MainMenu MenuObject = new MainMenu();
+        MenuObject.setVisible(true);
     }
 }
